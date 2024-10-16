91fad6a380a016e229373e582b427e574e767ac4
Alexander Saenen <alexdev.saenen@gmail.com>
2024-10-17 00:43:37 +0200

    fix: add cjs

---
+++ cjs/components/navbar/context.js
@@ -0,0 +1,16 @@
+"use strict";
+'use client';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ShowContext = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var ShowContext = /*#__PURE__*/_react["default"].createContext(false);
+
+exports.ShowContext = ShowContext;
+//# sourceMappingURL=context.js.map
